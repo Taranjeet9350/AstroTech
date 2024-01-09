76361c7db04966a601852131a5b96f97b0abed83
Srinjay-hack <srinjaymondal@gmail.com>
2024-01-09 19:52:53 +0530

    new

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Whyastro from "./WhyAstro/WhyAstro";
-
+import Navbar from "./common/Navbar/Navbar";
+import { BrowserRouter } from "react-router-dom";
 function App() {
   return (
     <div className="AstroTech">
--- src/WhyAstro/AstrologerCard.js
+++ src/WhyAstro/AstrologerCard.js
@@ -4,24 +4,24 @@ import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 
 function AstrologerCard() {
-    return (
-        <div  className="DivCard">
-            <div className="AboveCard">
-              <Card className="Card">
-                    <Card.Img variant="top" className="img" src="https://astrologerpnrao.com/wp-content/uploads/2023/03/WhatsApp-Image-2023-02-18-at-10.15.48-AM-576x576-1.webp" />
-                    <Card.Body>
-                    <Card.Title>Card Title</Card.Title>
-                    <Card.Text>
-                    Some quick example text to build on the card title and make up the
-                    bulk of the card's content.
-                    </Card.Text>
-                    <Button variant="primary">Go somewhere</Button>
-                    </Card.Body>
-              </Card>
-            
+  return (
+    <div className="DivCard">
+      <div className="AboveCard">
+        <Card className="Card">
+          <Card.Img variant="top" className="img" src="https://astrologerpnrao.com/wp-content/uploads/2023/03/WhatsApp-Image-2023-02-18-at-10.15.48-AM-576x576-1.webp" />
+          <Card.Body>
+            <Card.Title>Card Title</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </Card.Text>
+            <Button variant="primary">Go somewhere</Button>
+          </Card.Body>
+        </Card>
+
       </div>
     </div>
-    );
+  );
 }
 
 export default AstrologerCard;
