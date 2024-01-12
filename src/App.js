cf6d8963201df80f0d1b2d1929a44f90d2c7d497
Srinjay-hack <srinjaymondal@gmail.com>
2024-01-12 21:20:59 +0530

    login

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
 import React from "react";
 import Whyastro from "./WhyAstro/WhyAstro";
 import Navbar from "./common/Navbar/Navbar";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter,Route,Routes } from "react-router-dom";
+import LoginPage from "./pages/Login";
+import SignupPage from "./pages/Signup";
 function App() {
   return (
     <div className="AstroTech">
-    
+     <div className="min-h-full h-screen flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+     <div className="max-w-md w-full space-y-8">
       <BrowserRouter>
         <Navbar />
+        <Routes>
+            <Route path="/" element={<Whyastro/>} />
+            <Route path="/login" element={<LoginPage/>} />
+            <Route path="/signup" element={<SignupPage/>} />
+        </Routes>
       </BrowserRouter>
-
-     
-   <Whyastro/>
+      </div>
+      </div>
    </div>
    
   );
